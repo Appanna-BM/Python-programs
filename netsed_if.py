c0daadc6e6c279c016b7d5c64880684171a7fcbd
Appanna <appannabm26@gmail.com>
2026-08-17 22:31:36 +0530

    while loop program

--- netsed_if.py
+++ netsed_if.py
@@ -1,7 +1,7 @@
 country=input("Enter your country :")
 
 
-if country.strip().lower()=="india": # for clean input remove the white space using strip() and convert to lower char 
+if country.strip().lower()=="india": # for clean input remove the white space using strip() and convert to lower char
     age = int(input('Enter your age :'))
     if age >= 18:
         print("You are an adult")
---
+++ while_loop.py
@@ -0,0 +1,7 @@
+num = int(input("enter a number less than or equal to 10 :"))
+while num <=10 :
+    print(num)
+    num+=1
+print("Number is greater than 10")
+
+
