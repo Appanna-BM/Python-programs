4f18a7c5a9534cfd4b6c0baf4558350e55328980
Appanna <appannabm26@gmail.com>
2026-08-18 23:33:05 +0530

    nested loops

---
+++ nested_loops.py
@@ -0,0 +1,16 @@
+colors=["red","green"]
+items=["apple","guava"]
+
+for color in colors:
+    for item in items:
+        print(f"{item}:{color}")
+
+
+
+i = 1
+while i<=2:
+    j=1
+    while j<= 3:
+        print(f"i={i}, j={j}")
+        j+=1
+    i+=1
