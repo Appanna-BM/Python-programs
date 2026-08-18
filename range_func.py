6754009c3a61b2ada7c4f1fc257df7cfabcf2b6c
Appanna <appannabm26@gmail.com>
2026-08-18 23:05:45 +0530

    range function with for

---
+++ range_func.py
@@ -0,0 +1,8 @@
+for n in range(2, 5):#it excludes 5
+    print(n)
+
+for i in range(5, 0, -1):# reverse counting
+    print(i)
+
+for i in range(5):# stops before 5 that is it does not include 5
+    print(i)
